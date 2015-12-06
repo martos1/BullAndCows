49c7b4c80c780732af58cd7372b8cd2a14c532c8
martos1 <marto.132@gmail.com>
2015-12-06 15:43:36 +0200

    Update game.php

--- game.php
+++ game.php
@@ -1,27 +1,27 @@
 <?php
 require_once'readline.php';
 require_once'function.php'; 
-$firstUser = readline('Èãðà÷ 1 âúâåäè 4 öèôðåíî ÷èñëî: ');
+$firstUser = readline('Играч 1 въведи 4 цифрено число: ');
 echo str_repeat(PHP_EOL, 30);
-$secondUser = readline('Èãðà÷ 2 âúâåäè 4 öèôðåíî ÷èñëî: ');
+$secondUser = readline('Играч 2 въведи 4 цифрено число: ');
 echo str_repeat(PHP_EOL, 30);
 
 while(true){
-	echo str_repeat('-', 30) , PHP_EOL , '---------- Èãðà÷ 1: ----------', PHP_EOL;
-	$firstNum = readline('Âúâåäè ïðåäïîëàãàåìî ÷èñëî:');
+	echo str_repeat('-', 30) , PHP_EOL , '---------- Играч 1: ----------', PHP_EOL;
+	$firstNum = readline('Въведи предполагаемо число:');
 	$arr = checkNum($secondUser, $firstNum);
-	echo 'Áèêîâå:', $arr['bull'], PHP_EOL, 'Êðàâè:', $arr['cow'], PHP_EOL;
+	echo 'Бикове:', $arr['bull'], PHP_EOL, 'Крави:', $arr['cow'], PHP_EOL;
 	if($arr['bull'] == 4){
-		echo 'ÏÎÁÅÄÈÒÅË!';
+		echo 'ПОБЕДИТЕЛ!';
 		break;
 	}
 // Second gamer
-	echo str_repeat('-', 30) , PHP_EOL , '---------- Èãðà÷ 2: ----------', PHP_EOL;
-	$secondNum = readline('Âúâåäè ïðåäïîëàãàåìî ÷èñëî:');
+	echo str_repeat('-', 30) , PHP_EOL , '---------- Играч 2: ----------', PHP_EOL;
+	$secondNum = readline('Въведи предполагаемо число:');
 	$arr = checkNum($firstUser, $secondNum);
-	echo 'Áèêîâå:', $arr['bull'], PHP_EOL, 'Êðàâè:', $arr['cow'], PHP_EOL;
+	echo 'Бикове:', $arr['bull'], PHP_EOL, 'Крави:', $arr['cow'], PHP_EOL;
 	if($arr['bull'] == 4){
-		echo 'ÏÎÁÅÄÈÒÅË!';
+		echo 'ПОБЕДИТЕЛ!';
 		break;
 	}
 }
